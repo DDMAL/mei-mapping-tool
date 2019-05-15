40284af9794c8b32c62325f7ac1a3727ee068c3a
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-05-15 13:55:26 -0400

    added log-in feature
    log-in log-out feature with admin and user with mongodb. Also changed the route of the website for the login to be the first page and submitting to the simssa app

--- Website_SIMSSA/myapp/routes/index.js
+++ Website_SIMSSA/myapp/routes/index.js
@@ -2,8 +2,49 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/meiMapping', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET Userlist page. */
+router.get('/', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('user', {
+            "user" : docs
+        });
+    });
+});
+
+/* POST to Add User Service */
+router.post('/project', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+
 });
 
 module.exports = router;
