7b250d08178195d647c33330f1b323f166c3149a
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-05-15 12:05:09 -0400

    Added users route and changed mei-encoding logic

--- Website_SIMSSA/myapp/app.js
+++ Website_SIMSSA/myapp/app.js
@@ -21,8 +21,8 @@ app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 
-app.use('/', indexRouter);
-//app.use('/users', usersRouter);
+app.use('/mei-encoding', indexRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
