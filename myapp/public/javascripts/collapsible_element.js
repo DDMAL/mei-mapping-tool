29a990f7bbb42121b65594bf2328e12e595f1609
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-05-28 16:40:40 -0400

    Added the pop-up with the new jquery implementation

--- myapp/public/javascripts/collapsible_element.js
+++ myapp/public/javascripts/collapsible_element.js
@@ -1,34 +1,10 @@
 
 /////Jquery to add multiple items and keep the event listeners
 var $addButton = $(".z_plus");
-$addButton.on("click", function(e) {
-    e.preventDefault();
-    var $cloner = $(this).closest(".clone");    
-    $cloned.clone(true, true).insertAfter($cloner);
-    var $classifier = $('.glyphicon.glyphicon-plus')
-    
-    //Any button with a black + will be able to add input fields
-    $classifier.on("click", function(e) {  
-      $('<input>').insertBefore(this);
-    });
-   
-    //Adding ace editors each time the button is pressed :
-    var $editor = ace.edit("editor");
-    $editor.setTheme("ace/theme/monokai");
-    $editor.getSession().setMode("ace/mode/javascript");
-
-   
-    $("<div>").dropzone({ url: "/file/post" }).insertBefore(this);
-
-});
-var $cloned = $('.clone').clone(true, true);
-
-//Adding css files : 
-$('head').append('<link rel="stylesheet" href="/stylesheets/dropzone.css" type="text/css" />');
-/////Modal button with the submit button
-
+// Get the modal and cloned element
 // Get the modal and cloned element
 var modal = document.getElementById("myModal");
+ 
 
 // Get the button that opens the modal
 var btn = document.getElementById("button");
@@ -52,6 +28,42 @@ window.onclick = function(event) {
   }
 }
 
- var editor = ace.edit("editor");
-  editor.setTheme("ace/theme/monokai");
-  editor.getSession().setMode("ace/mode/javascript");
+$addButton.on("click", function(e) {
+
+//Getting the elements for the collapsible element:
+var btn = $("#button");
+// Get the <span> element that closes the modal
+var span = $(".close")[0];
+  //Open modal : 
+  btn.onclick = function() {
+  modal.style.display = "block";
+}
+//Closing the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+    e.preventDefault();
+    var $cloner = $(this).closest(".clone");    
+    $cloned.clone(true, true).insertAfter($cloner);
+    var $classifier = $('.glyphicon.glyphicon-plus')
+    
+    //Any button with a black + will be able to add input fields
+    $classifier.on("click", function(e) {  
+      $('<input>').insertBefore(this);
+    });
+    
+
+
+});
+var $cloned = $('.clone').clone(true, true);
+
+var $collapsibleButton = $("#collapsibleButton");
+
+
