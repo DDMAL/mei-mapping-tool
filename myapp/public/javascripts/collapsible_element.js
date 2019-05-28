4c326df1624a03c9a211644f0bba53233faf2e5e
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-05-28 15:00:33 -0400

    Added the forms who have different values from the parent forms
    Must have jquery in npm install

--- myapp/public/javascripts/collapsible_element.js
+++ myapp/public/javascripts/collapsible_element.js
@@ -1,142 +1,57 @@
-//add function for Ace editor : 
 
-  var coll = document.getElementsByClassName("collapsible");
-  var i;
-  for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-  this.classList.toggle("active");
-  var content = this.nextElementSibling;
-  if (content.style.display === "block") {
-  content.style.display = "none";
-  } else {
-  content.style.display = "block";
-  }
-  });
-  }
+/////Jquery to add multiple items and keep the event listeners
+var $addButton = $(".z_plus");
+$addButton.on("click", function(e) {
+    e.preventDefault();
+    var $cloner = $(this).closest(".clone");    
+    $cloned.clone(true, true).insertAfter($cloner);
+    var $classifier = $('.glyphicon.glyphicon-plus')
+    
+    //Any button with a black + will be able to add input fields
+    $classifier.on("click", function(e) {  
+      $('<input>').insertBefore(this);
+    });
+   
+    //Adding ace editors each time the button is pressed :
+    var $editor = ace.edit("editor");
+    $editor.setTheme("ace/theme/monokai");
+    $editor.getSession().setMode("ace/mode/javascript");
+
+   
+    $("<div>").dropzone({ url: "/file/post" }).insertBefore(this);
+
+});
+var $cloned = $('.clone').clone(true, true);
+
+//Adding css files : 
+$('head').append('<link rel="stylesheet" href="/stylesheets/dropzone.css" type="text/css" />');
+/////Modal button with the submit button
 
 // Get the modal and cloned element
- var modal = document.getElementById("myModal");
- 
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
- var btn = document.getElementById("button");
- var buttonSubmit = document.getElementById("newCollapsible")
+var btn = document.getElementById("button");
+var buttonSubmit = document.getElementById("newCollapsible")
 
 // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-modal.style.display = "block";
+  modal.style.display = "block";
 }
 span.onclick = function() {
-modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-if (event.target == modal) {
-  modal.style.display = "none";
-}
-}
-
-////Submit button clicked events for creating a new element
-buttonSubmit.onclick = function() {
-
-//Submit button closes pop-up
- modal.style.display = "none";
-
-
-//Creating a button element
- var name = document.getElementById("elementName").value;
-
- //Value of the container for the dropzone : 
- var dropContainer = document.getElementById("hey");
-// var buttonCollapse = document.createElement("BUTTON");
- //var collapsibleAttribute = document.createAttribute("class");
- //collapsibleAttribute.value = "collapsible";
- //buttonCollapse.setAttributeNode(collapsibleAttribute);
-var buttonClone = document.getElementById("collapsibleButton");
-var buttonCloned = buttonClone.cloneNode(true);
-buttonCloned.innerText = name;
-
-var boxContainer = document.getElementById("roundedBoxId");
-boxContainer.appendChild(buttonCloned); //This works!
-
-
-//Appending it to the bounding box for the collapsible
- buttonCloned.innerText = name;
- //var boxContainer = document.getElementById("roundedBoxId");
- //boxContainer.appendChild(buttonCloned);
-
-//Submit button adds a new collapsible element
-//var name = document.getElementById("elementName").value; //name submitted by the person
-/*var buttonClone = document.getElementById("collapsibleButton");
-var buttonCloned = buttonClone.cloneNode(true);
-buttonCloned.innerText = name;
-var boxContainer = document.getElementById("roundedBoxId");
-boxContainer.appendChild(buttonCloned);
-//document.getElementById("collapsibleButton").innerText = name;*/
-
-//Maling the element for cloning the content of the collapsible
-var meiEncodingToClone = document.getElementById("template");
-document.getElementsByName("input").innerHTML = " ";
-var meiEncodingCloned = meiEncodingToClone.cloneNode(true);
-var elementCloned = document.getElementById("template");
-//document.getElementsByName("input").value = "";
-//Changing the background of the element collapsible
-meiEncodingCloned.style.backgroundColor = "#f1f1f1";
-boxContainer.appendChild(meiEncodingCloned);
-
-var createDropzone = document.getElementById("mydiv");
-//meiEncodingCloned.appendChild(createDropzone);
-
-//Event listener for the click button of the collapsible element
-buttonCloned.addEventListener("click", function() {
-  this.classList.toggle("active");
-  var content = meiEncodingCloned;
-  if (content.style.display === "block") {
-  content.style.display = "none";
-  } else {
-  content.style.display = "block";
-  }
-  });
-meiEncodingCloned
-//Appending the meiEncodingCloned to the document
-boxContainer.appendChild(meiEncodingCloned);
-
-//Making dividers for the dropzone : 
-
-//Dropzone element multiple times
-var x = document.getElementsByName("hey");
-var i;
-  for (i = 0; i < x.length; i++) {
-      x[i].onclick = function() {
-     var divisionDrop = document.createElement("div");
-     var divisionId = document.createAttribute("id");
-     divisionId.value = x[i];
-     divisionDrop.setAttributeNode(divisionId);
-     var formDropzone = document.getElementsByName("sectionDrop");
-     document.body.appendChild(divisionDrop);
-    // Dropzone class:
-    var myDropzone = new Dropzone(divisionDrop, { url: "/file/post"});
-
-    //Shutting off the autodiscovery element of the specific elements 
-    Dropzone.autoDiscover = false; 
-}
-}}
-
-//Delete elements
-var closebtns = document.getElementsByClassName("close");
-var i;
-
-for (i = 0; i < closebtns.length; i++) {
-  closebtns[i].addEventListener("click", function() {
+  if (event.target == modal) {
     modal.style.display = "none";
-  });
-
+  }
 }
 
-
-//Function to create a new Collapsible object 
-
+ var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/javascript");
---
+++ myapp/public/javascripts/hiddenDropzone.js
@@ -0,0 +1,5 @@
+
+  if(document.getElementById("undefined") != null){
+  	var dropzoneCopy = document.getElementById("undefined");
+  	dropzoneCopy.className = "dropzone dz-clickable dz-started";
+  }
