b005bd28df83b376679a0ad19b69f43bfb872584
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-05-29 10:35:20 -0400

    Collapsibles work with the new addition

--- myapp/public/javascripts/collapsible_element.js
+++ myapp/public/javascripts/collapsible_element.js
@@ -14,10 +14,10 @@ for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
+    if (content.style.display === "none") {
       content.style.display = "block";
+    } else {
+      content.style.display = "none";
     }
   });
 }
