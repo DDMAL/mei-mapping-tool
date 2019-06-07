073cb3439c06b243e8a29d06ad934a0782b69afd
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-06-07 12:12:34 -0400

    Dropzone is working and uploading to uploads file!

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ var router = express.Router();
 var auth = require("../controllers/AuthController.js");
 
 // restrict index for logged in user only
-router.get('/', auth.home);
-
+router.get('/', function (req, res) {
+    res.send('<html><head><title>Dropzone example</title><link href="/stylesheets/dropzone.css" rel="stylesheet"></head><body><h1>Using Dropzone</h1><form method="post" action="/" class="dropzone" id="dropzone-example"><div class="fallback"><input name="file" type="file" multiple /></div></form><p><a href="/old">Old form version</a></p><script src="/javascripts/dropzone.js"></script></body></html>');
+});
 // route to register page
 router.get('/register', auth.register);
 
--- routes/upload.js
+++ routes/upload.js
@@ -1,36 +1,12 @@
+var express = require('express');
+var router = express.Router();
 
-const multer = require('multer');
-const path   = require('path');
-
-/** Storage Engine */
-const storageEngine = multer.diskStorage({
-  destination: './public/files',
-  filename: function(req, file, fn){
-    fn(null,  new Date().getTime().toString()+'-'+file.fieldname+path.extname(file.originalname));
-  }
-}); 
-
-//init
-
-const upload =  multer({
-  storage: storageEngine,
-  limits: { fileSize:200000 },
-  fileFilter: function(req, file, callback){
-    validateFile(file, callback);
-  }
-}).single('photo');
-
-
-var validateFile = function(file, cb ){
-  allowedFileTypes = /jpeg|jpg|png|gif/;
-  const extension = allowedFileTypes.test(path.extname(file.originalname).toLowerCase());
-  const mimeType  = allowedFileTypes.test(file.mimetype);
-  if(extension && mimeType){
-    return cb(null, true);
-  }else{
-    cb("Invalid file type. Only JPEG, PNG and GIF file are allowed.")
-  }
-}
-
-
-module.exports = upload;
+/* GET users listing. */
+router.post('/', function(req, res) {
+  console.log('hey'); // the uploaded file object
+});
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+module.exports = router;
