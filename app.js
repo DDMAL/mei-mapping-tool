3a03ad0104edf7178f2803b3dcdbf1aa5c73d301
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-06-07 14:14:02 -0400

    Added dropzone post methods

--- app.js
+++ app.js
@@ -38,6 +38,31 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize())  
 app.use(passport.session()) 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(multer({dest: 'uploads'}).any()); // dest is not necessary if you are happy with the default: /tmp
+app.use(express.static(path.join(__dirname, 'bower_components')));
+// routes
+app.get('/', function (req, res) {
+    res.send('<html><head><title>Dropzone example</title><link href="/dropzone/downloads/css/dropzone.css" rel="stylesheet"></head><body><h1>Using Dropzone</h1><form method="post" action="/" class="dropzone" id="dropzone-example"><div class="fallback"><input name="file" type="file" multiple /></div></form><p><a href="/old">Old form version</a></p><script src="/dropzone/downloads/dropzone.js"></script></body></html>');
+});
+
+app.get('/old', function (req, res) {
+    res.send('<html><head><title>Dropzone example</title><link href="/dropzone/downloads/css/dropzone.css" rel="stylesheet"></head><body><h1>Old form</h1><form method="post" action="/" id="old-example" enctype="multipart/form-data"><input name="file" type="file" multiple /><button>Save</button></form><script src="/dropzone/downloads/dropzone.js"></script></body></html>');
+});
+
+app.post('/', function (req, res) {
+    //console.log(req.files);
+        var files = req.files.file;
+    if (Array.isArray(files)) {
+        // response with multiple files (old form may send multiple files)
+        console.log("Got " + files.length + " files");
+    }
+    else {
+        // dropzone will send multiple requests per default
+        console.log("Got one file");
+    }
+    res.sendStatus(200);
+});
 
 app.use('/', routes);
 app.use('/neumes', neumes);
