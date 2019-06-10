d98459458745be6ebc44d99423dd32a1f21fc5cb
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-06-10 16:40:38 -0400

    Added editor registration

--- routes/neumes.js
+++ routes/neumes.js
@@ -15,10 +15,35 @@ router.use(methodOverride(function(req, res){
         return method
       }
 }))
+router.route('/editor')
+    //GET all neumes
+    .get(function(req, res, next) {
+        //retrieve all neumes from Mongo
+        mongoose.model('neume').find({}, function (err, neumes) {
+              if (err) {
+                  return console.error(err);
+              } else {
+                  //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+                  res.format({
+                      //HTML response will render the index.jade file in the views/neumes folder. We are also setting "neumes" to be an accessible variable in our jade view
+                    html: function(){
+                        res.render('neumes/editor', {
+                              title: 'Neumes',
+                              "neumes" : neumes
+                          });
+                    },
+                    //JSON response will show all neumes in JSON format
+                    json: function(){
+                        res.json(neumes);
+                    }
+                });
+              }     
+        });
+    })
 router.route('/user')
     //GET all neumes
     .get(function(req, res, next) {
-        //retrieve all neumes from Monogo
+        //retrieve all neumes from Mongo
         mongoose.model('neume').find({}, function (err, neumes) {
               if (err) {
                   return console.error(err);
--- routes/router.js
+++ routes/router.js
@@ -37,20 +37,25 @@ router.post('/', function (req, res, next) {
         return next(error);
       } else {
         req.session.userId = user._id;
+        if(req.body.role == "editor")
+          return res.redirect("neumes/editor");
         return res.redirect('/neumes');
       }
     });
 
   } else if (req.body.logemail && req.body.logpassword) {
+    
     User.authenticate(req.body.logemail, req.body.logpassword, function (error, user) {
       if (error || !user) {
         var err = new Error('Wrong email or password.');
         err.status = 401;
         return next(err);
       } else {
+
         req.session.userId = user._id;
         return res.redirect('/neumes');
       }
+
     });
   } else {
     var err = new Error('All fields required.');
