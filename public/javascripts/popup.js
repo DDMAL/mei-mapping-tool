c321b44ad831def634663d0fe65a90dd72c8c123
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-04 09:25:10 -0400

    Added edit images modals
    For editing the images

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -1,27 +1,29 @@
-// Get the modal
-window.onload = function(){ 
-var modal = document.getElementById("myModal");
+// Function to initialise modals
+function initializeModal(modalID, buttonID) {
+  //Modal id not getting the information
+  // Get the modal element
+  var modal = document.getElementById(modalID);
 
-// Get the button that opens the modal
-var btn = document.getElementById("newNeumeButton");
+  // Get the button that opens the modal
+  var btn = document.getElementById(buttonID);
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = modal.querySelector('.close');
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+  // When the user clicks on the button, open the modal
+  btn.addEventListener('click', function() {
+    modal.style.display = "block";
+  });
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+  // When the user clicks on <span> (x), close the modal
+  span.addEventListener('click', function() {
     modal.style.display = "none";
-  }
+  });
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.addEventListener('click', function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
 }
-};
