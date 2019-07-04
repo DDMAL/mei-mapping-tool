113a239762ad3967de08a13f0ae132c2fdf3c813
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-04 08:36:55 -0400

    Fixed collapsible bug

--- routes/neumes.js
+++ routes/neumes.js
@@ -8,6 +8,8 @@ global.neumes_array = [];
 
 //Any requests to this controller must pass through this 'use' function
 //Copy and pasted from method-override
+
+//Change this for the users to go to the projects page
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(methodOverride(function(req, res){
       if (req.body && typeof req.body === 'object' && '_method' in req.body) {
