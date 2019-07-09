a64df406f38f3a45a8164a8d55f9a16530cb3284
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-09 15:29:38 -0400

    Added username to the abouts page

--- routes/neumes.js
+++ routes/neumes.js
@@ -44,33 +44,6 @@ router.route('/editor')
               }     
         });
     })
-
-router.route('/user')
-    //GET all neumes
-    .get(function(req, res, next) {
-        //retrieve all neumes from Mongo
-        mongoose.model('neume').find({}, function (err, neumes) {
-              if (err) {
-                  return console.error(err);
-              } else {
-
-                  //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
-                  res.format({
-                      //HTML response will render the index.jade file in the views/neumes folder. We are also setting "neumes" to be an accessible variable in our jade view
-                    html: function(){
-                        res.render('neumes/user', {
-                              title: 'Neumes',
-                              "neumes" : neumes
-                          });
-                    },
-                    //JSON response will show all neumes in JSON format
-                    json: function(){
-                        res.json(neumes);
-                    }
-                });
-              }     
-        });
-    })
     
 //build the REST operations at the base for neumes
 //this will be accessible from http://127.0.0.1:3000/neumes if the default route for / is left unchanged
--- routes/router.js
+++ routes/router.js
@@ -11,10 +11,33 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.get('/', function (req, res, next) {
   return res.render('index');
 });
+var userFinal = [];
 /* GET New project page. */
 router.get('/about', function(req, res) {
-     res.render('about', { title: 'About' });
-     //Adding the new names lines
+      mongoose.model('User').find({_id : req.session.userId}, function (err, users) { 
+                    userFinal = users;
+                  });
+                mongoose.model('User').find({_id : req.session.userId}, function (err, users) { 
+                userFinal = users;
+               // console.log(userFinal);//This works!!!
+              });
+                  //respond to both HTML and JSON. JSON responses require 'Accept: application/json;' in the Request Header
+                  res.format({
+                      //HTML response will render the index.jade file in the views/projects folder. We are also setting "projects" to be an accessible variable in our jade view
+                    html: function(){
+                      console.log(userFinal);
+                        res.redirect('/about', {
+                              title: 'About',
+                              "users" : userFinal
+                          });
+                    },
+                    //JSON response will show all projects in JSON format
+                    json: function(){
+                        res.json(projects);
+                    }
+                }); 
+              global.userFinal = []; //The user needs to be added in all the routes
+
 });
 
 //POST route for updating data
