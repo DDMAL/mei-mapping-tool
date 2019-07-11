e4b1fe92f6ccf835cc66d4d600cea21f9b479cbf
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-11 14:01:00 -0400

    Took away the dropzone remove links

--- public/javascripts/dropzone.js
+++ public/javascripts/dropzone.js
@@ -369,7 +369,7 @@ var Dropzone = function (_Emitter) {
          * already uploading) the file. The `dictCancelUpload`, `dictCancelUploadConfirmation`
          * and `dictRemoveFile` options are used for the wording.
          */
-        addRemoveLinks: true,
+        addRemoveLinks: false,
 
         /**
          * Defines where to display the file previews – if `null` the
@@ -778,7 +778,7 @@ var Dropzone = function (_Emitter) {
               node.innerHTML = this.filesize(file.size);
             }
 
-            if (true) {
+            if (false) {
               file._removeLink = Dropzone.createElement("<a class=\"dz-remove\" href=\"javascript:undefined;\" data-dz-remove>" + this.options.dictRemoveFile + "</a>");
               file.previewElement.appendChild(file._removeLink);
             }
