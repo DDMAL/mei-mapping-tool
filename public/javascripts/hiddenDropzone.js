6e1139a0be43175f47faabd17545bd953bf2ae6c
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-11 15:57:21 -0400

    Deleted unused javascript files

--- public/javascripts/hiddenDropzone.js
+++
@@ -1,5 +0,0 @@
-
-  if(document.getElementById("undefined") != null){
-  	var dropzoneCopy = document.getElementById("undefined");
-  	dropzoneCopy.className = "dropzone dz-clickable dz-started";
-  }
