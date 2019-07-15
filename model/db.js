5f425a4924b27d9728ddb2151755f33a31cc72e4
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-15 08:52:19 -0400

    changed port for mongodb to connect

--- model/db.js
+++ model/db.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose');
 
-mongoose.createConnection('mongodb://132.206.14.234:8800/mei-mapping-tool');
+mongoose.createConnection('mongodb://132.206.14.234:27017/mei-mapping-tool');
 
