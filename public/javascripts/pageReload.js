bced7bd624b2d6abd4f17992aaea5ff6d1359731
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-17 10:10:11 -0400

    Took away the page reload

--- public/javascripts/pageReload.js
+++ public/javascripts/pageReload.js
@@ -1,11 +1,2 @@
 //add this js script into the web page,
 //you want reload once after first load
-window.onload = function() {
-    //considering there aren't any hashes in the urls already
-    if(!window.location.hash) {
-        //setting window location
-        window.location = window.location + '#loaded';
-        //using reload() method to reload web page
-        window.location.reload();
-    }
-}
