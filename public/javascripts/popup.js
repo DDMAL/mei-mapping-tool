75c112765d9692845b1af90c693d1f300abee4eb
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-19 13:22:16 -0400

    Added popup code to the upload csv files

--- public/javascripts/popup.js
+++ public/javascripts/popup.js
@@ -17,13 +17,34 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
+
+var uploadModal = document.getElementById("uploadCSV");
+
+// Get the button that opens the modal
+var btnCSV = document.getElementById("uploadCSVButton");
+
+// Get the <span> element that closes the modal
+var spanCSV = document.getElementsByClassName("close")[1];
+// When the user clicks on the button, open the modal 
+btnCSV.onclick = function() {
+  uploadModal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+spanCSV.onclick = function() {
+  uploadModal.style.display = "none";
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
+  if (event.target == uploadModal) {
+     uploadModal.style.display = "none";
+  }
   if (event.target == modal) {
      modal.style.display = "none";
   }
 }
 };
+
 function initializeModal(modalID, buttonID) {
   //Modal id not getting the information
   // Get the modal element
