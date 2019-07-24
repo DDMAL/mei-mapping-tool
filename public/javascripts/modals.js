8440d6ffd2ccb8386ebcf5fb47d4600e103cc957
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-24 10:55:24 -0400

    Added a fix to the popup

--- public/javascripts/modals.js
+++ public/javascripts/modals.js
@@ -16,11 +16,8 @@ function initializeModal(modalID, buttonID) {
     modal.style.display = "block";
   });
   
-
   // When the user clicks on <span> (x), close the modal
-  span.addEventListener('click', function() {
-    modal.style.display = "none";
-  });
+
 
   // When the user clicks anywhere outside of the modal, close it
   window.addEventListener('click', function(event) {
