9bc970eed3dc517065771ad25451bcd27c8570ac
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-07-26 15:01:36 -0400

    Added refresh page

---
+++ public/javascripts/refresh.js
@@ -0,0 +1,13 @@
+ var time = new Date().getTime();
+     $(document.body).bind("mousemove keypress", function(e) {
+         time = new Date().getTime();
+     });
+
+     function refresh() {
+         if(new Date().getTime() - time >= 300000) 
+             window.location.reload(true);
+         else 
+             setTimeout(refresh, 300000);
+     }
+
+     setTimeout(refresh, 300000);
