8a9cc08db6047d5bf0494e12e82464a34a8c3bce
ImaneChafi <46538274+ImaneChafi@users.noreply.github.com>
2019-08-01 14:02:45 -0400

    Fixed bug with delete button
    When adding the new feature to save all, the delete button stopped working.

---
+++ public/javascripts/scroll-keep.js
@@ -0,0 +1,11 @@
+function refreshPage () {
+    var page_y = document.getElementsByTagName("body")[0].scrollTop;
+    window.location.href = window.location.href.split('?')[0] + '?page_y=' + page_y;
+}
+window.onload = function () {
+    setTimeout(refreshPage, 35000);
+    if ( window.location.href.indexOf('page_y') != -1 ) {
+        var match = window.location.href.split('?')[1].split("&")[0].split("=");
+        document.getElementsByTagName("body")[0].scrollTop = match[1];
+    }
+}
