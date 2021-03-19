ed0f060f6b474253a37b967ff7b25755b09896a6
evansavage <esav678@gmail.com>
2021-03-19 10:12:37 -0400

    Added new LuckySheet template

--- routes/projects.js
+++ routes/projects.js
@@ -465,7 +465,7 @@ router.route('/project/:id') //Doesnt lead to the projects page, see projects/:i
                 projectdob = projectdob.substring(0, projectdob.indexOf('T'))
                 res.format({
                     html: function() {
-                        res.render('projects/show', {
+                        res.render('projects/showLuckySheet', {
                             "projectdob": projectdob,
                             "project": project,
                             "neumes": ArrayNeumes
@@ -719,7 +719,7 @@ router.route('/:id') //This is where the classifier would be
                             res.format({
                                 html: function() {
 
-                                    res.render('projects/show', {
+                                    res.render('projects/showLuckySheet', {
                                         "projectdob": projectdob,
                                         "project": project,
                                         "neumes": neumeFinal,
