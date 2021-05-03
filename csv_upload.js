2fd09ed0f716b1bb55f19f402bb13f99fd5f8523
evansavage <esav678@gmail.com>
2021-05-03 12:49:33 -0400

    Successfully adding neume data from XLSX format on server

--- csv_upload.js
+++ csv_upload.js
@@ -2,23 +2,67 @@ const node_xj = require("xls-to-json");
 const xlsx = require("xlsx");
 const ExcelJS = require('exceljs');
 
-function xlsxAdd(buffer) {
-  console.log('xlsx add');
+function xlsxAdd(buffer, projectID, socket) {
+  console.log(`xlsx add to ${projectID}`);
   const workbook = new ExcelJS.Workbook();
+  const columnNames = [
+    {header: 'images', key: 'images'},
+    {header: 'name', key: 'name'},
+    {header: 'genericName', key: 'genericName'},
+    {header: 'folio', key: 'folio'},
+    {header: 'description', key: 'description'},
+    {header: 'classification', key: 'classification'},
+    {header: 'encoding', key: 'encoding'},
+  ]
+
   console.log('good so far!')
   workbook.xlsx.load(buffer).then(wb => {
-    console.log(workbook, 'workbook instance');
+    // console.log(workbook, 'workbook instance');
     workbook.eachSheet((sheet, id) => {
-      var neume = {};
+      var neumes = [];
+      var keys = ['images']
+      sheet.columns = columnNames;
       sheet.eachRow((row, index) => {
-        console.log(row.values, index);
-        
+        var neume = {}
+        console.log(index);
+        if (index - 1) {
+          columnNames.forEach((name, i) => {
+            neume[name['key']] = row.getCell(name['key']).value;
+          })
+          neumes.push(neume);
+        }
       })
       for (const image of sheet.getImages()) {
         console.log('processing image row', image.range.tl.nativeRow, 'col', image.range.tl.nativeCol, 'imageId', image.imageId);
         const img = workbook.model.media.find(m => m.index === image.imageId);
-        console.log(img.buffer);
+        // console.log(img.buffer);
+        neumes[image.range.tl.nativeRow - 1]['images'] = img.buffer;
       }
+      neumes.forEach((neume, index) => {
+        mongoose.model('neume').create({
+            name: neume['name'],
+            folio: neume['folio'],
+            description: neume['description'],
+            classification: neume['classification'],
+            mei: neume['encoding'],
+            review: '',
+            dob: '',
+            imagePath: '',
+            project: projectID,
+            neumeSection: '',
+            neumeSectionName: '',
+            source: '',
+            genericName: neume['genericName']
+
+        }, function(err, neume) {
+            if (err) {
+                return renderError(res, err);
+            } else {
+              console.log(`message: created neume in project id: ${projectID}\t\t\tneume id: ${neume._id}`);
+              socket.emit('new neume info spreadsheet', [projectID, neume]);
+            }
+        })
+      })
     })
   })
   // let sheetData = xlsx.utils.sheet_to_json(workbook)
