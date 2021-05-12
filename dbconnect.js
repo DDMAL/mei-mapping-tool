159fe7d27b675c51d0dd41bdc5c310cb99e60c6c
evansavage <esav678@gmail.com>
2021-05-12 13:54:47 -0400

    Added code for mongo listening port

--- dbconnect.js
+++ dbconnect.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
-const connect = mongoose.connect('mongodb://localhost:27017/mei-mapping-tool', {useNewUrlParser:true});
+const mongo_port = process.env.MONGO_PORT || 27017;
+
+const connect = mongoose.connect(`mongodb://localhost:${mongo_port}/mei-mapping-tool`, {useNewUrlParser:true});
 
 module.exports = connect;
