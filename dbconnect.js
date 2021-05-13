abbfc7c6b4e740f0c52603fc6cfe1f87242ab1ee
evansavage <esav678@gmail.com>
2021-05-13 12:16:55 -0400

    Fixed conflicting mongodb connections

--- dbconnect.js
+++ dbconnect.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
 const mongo_port = process.env.MONGO_PORT || 27017;
 
-const connect = mongoose.connect(`mongodb://localhost:${mongo_port}/mei-mapping-tool`, {useNewUrlParser:true});
+const connect = mongoose.connect(`mongodb://localhost:${mongo_port}/mei-mapping-tool`,
+  {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+  });
 
 module.exports = connect;
